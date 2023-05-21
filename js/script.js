55cc04e42d53c800c8ca041e30d1e7cb3432de7b
Yurii-Br <127565054+Yurii-Br@users.noreply.github.com>
2023-05-21 17:43:00 +0200

    add

--- js/script.js
+++ js/script.js
@@ -1,5 +1,9 @@
 let offSet = 0;
 const sliderLine = document.querySelector(".container-img");
+const modalWind = document.querySelector(".modalwind");
+const openMod = document.querySelector(".btn-open");
+const closeMod = document.querySelector(".btn-close");
+const isClose = document.querySelector(".is-close");
 
 document.querySelector(".btn-next").addEventListener("click", function () {
   offSet = offSet + 250;
@@ -15,4 +19,11 @@ document.querySelector(".btn-prew").addEventListener("click", function () {
     offSet = 750;
   }
   sliderLine.style.left = -offSet + "px";
+});
+
+document.querySelector(".btn-open").addEventListener("click", function () {
+  modalWind.classList.remove("is-close");
+});
+document.querySelector(".btn-close").addEventListener("click", function () {
+  modalWind.classList.add("is-close");
 });
